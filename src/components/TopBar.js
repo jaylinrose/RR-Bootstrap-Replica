fa0d0a08c8348c78cf77a674c9a90d7537c8b663
jaylinrose <jaylin.manuel02@gmail.com>
2023-07-16 22:51:15 -0700

    fixed navbar in topBar.js bug and added bootstrap for background and storelisting

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,9 @@
-import NavBar from 'react-bootstrap/NavBar'
+import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
 
-
-export default function TopBar () {
-    return(
+export default function TopBar() {
+    return (
         <div>
             <Navbar bg="light" expand="lg" >
                 <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home"><img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" /> </Navbar.Brand>
@@ -19,7 +18,5 @@ export default function TopBar () {
                 </Navbar.Collapse>
             </Navbar>
         </div>
-       
     )
-
 }
